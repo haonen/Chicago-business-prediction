16b1577dab716c7c974d79bfe22c0146455063c3
Peng Wei <pengwei@cs.uchicago.edu>
2019-04-25 20:09:38 -0500

    add some code to download 311 data and acs data

--- pipeline/data_loader.py
+++ pipeline/data_loader.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 '''
 Load data from web
 '''
@@ -5,39 +6,81 @@ from sodapy import Socrata
 import pandas as pd
 import numpy as np
 import datetime as dt
+from census import Census
+from us import states
 
-COL_TYPES = {'block': str, 
-             'case_number': str,
-             'community_area': str,
-             'primary_type': 'category',
-             'date': str,
-             'latitude': float,
-             'longitude': float,
-             'year': int,
-             'ward': int}
-MAX_ROWS = 6839451 # the total rows of the original data
 
-CRIME_DATA_ID = "6zsd-86xi"
-
-def get_crime():
+def get_311(start_year, end_year):
     '''
-    Get 2017 and 2018 crime data from chicago data portal
+    Get 2012 and 2017 311 data from chicago data portal
+
+    Input:
+        start_year: timestamp '"2017-12-31T20:00:05.000"'format
+        end_time: timestamp  '"2017-12-31T20:00:05.000"' format
 
     Return:
         pandas dataframe with the columns and dtypes as COL_TYPES
-    '''
+    '''   
+    COL_TYPES = {'st_type': str, 
+                 'created_date': str,
+                 'zip_code': float,
+                 'longitude': float,
+                 'lattitude': int} 
+
+    DATA_ID = "v6vf-nfxy"
     cols = [item for item in COL_TYPES.keys()]
-    client = Socrata('data.cityofchicago.org',
-                     app_token,
-                     username=,
-                     password=)
-    conds = "year = 2017 or year = 2018"
-    res = client.get(CRIME_DATA_ID, 
+    client = Socrata('data.cityofchicago.org', 
+                     'Lfkp6VmeW3p5ePTv0GhNSmrWh',
+                     username="pengwei@uchciago.edu",
+                     password="2h1m@k@1men")
+
+    conds = "created_date between {} and {}"\
+            .format(start_year, end_year)  
+    res = client.get(DATA_ID, 
                      select=",".join(cols),
-                     where= conds,
-                     limit = MAX_ROWS)
+                     where= conds)
     client.close()
     df = pd.DataFrame.from_records(res)
-    df = df[df['ward'].notna()].astype(COL_TYPES)
-    df['date'] = pd.to_datetime(df['date'])
+    df = df.astype(COL_TYPES)
+    df['year'] = pd.to_datetime(df['created_date'])
     return df
+
+def get_acs_data(start_year, end_year):
+    '''
+    Get the information from census data
+    Total population, white population, black population,
+    high school degree population, household income
+
+    Return:
+        pandas dataframe
+    '''
+    NAMES_DIC = {'B01001_001E': "population", 
+             'B19013_001E': "median_household_income", 
+             'B19083_001E': "gini_index", 
+             'DP03_0009E': "unemployment_rate", 
+             'B992701_002E': "health_coverage_population",
+             'B07012_005E': 'same_house_one_year_ago',
+             'S1701_C03_046E':'poverty_rate'}  
+    c = Census('3eb1575454b4de2cf12e0072bd946ecb852579d2')
+    lst_df = []
+
+    for item in range(start_year, end_year+1):
+        res = c.acs5.get(('NAME', 
+                   'B01001_001E',
+                   'B19013_001E',
+                   'B19083_001E',
+                   #'DP03_0009E',
+                   'B992701_002E',
+                   'B07012_005E',
+                   #'S1701_C03_046E'
+                   ),
+                   {'for': 'block group',
+                   'in': 'state: {} county: {}'.format('17','031')},
+                   year = item)
+        df = pd.DataFrame.from_records(res)
+        df.rename(columns=NAMES_DIC,inplace=True)
+        df.drop(columns =['NAME'], axis=1, inplace=True)
+        df['geoid'] = df["state"] + df["county"] + df["tract"]
+        df['year'] = item
+        lst_df.append(df)
+    return pd.concat(lst_df)
