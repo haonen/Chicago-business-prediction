13cc83b7d04572b5596ca058c921c5b8db8f3566
haonen <38400231+haonen@users.noreply.github.com>
2019-05-27 22:28:19 -0500

    Add files via upload

---
+++ wrap_up/get_dummy.py
@@ -0,0 +1,64 @@
+import numpy as np
+import pandas as pd
+
+
+def get_all_dummies(X_train, X_test, colname):
+    '''
+    Convert the categorical variable into dummies
+    Inputs:
+        X_train: a data frame of training set
+        X_test: a data frame of test set
+        colname: the name of the colname
+    Return:
+        the data frame with those dummies into data frame
+    '''
+    # Get the categories from training data set
+    cat_list = list(X_train[colname].value_counts().index.values)
+    # create dummies
+    for cat in cat_list:
+        X_test[cat] = np.where(X_test[colname] == cat, 1, 0)
+        X_train[cat] = np.where(X_train[colname] == cat, 1, 0)
+
+
+def get_top_k_dummies(X_train, X_test, colname, k):
+    '''
+    For columns with too many categories, only create dummies for
+    top k categories
+    Inputs:
+        X_train: a data frame of training set
+        X_test: a data frame of test set
+        colname: the name of the column
+        k: (int) the value of k
+    Outputs:
+       Create dummies in both train and test set
+    '''
+    # get top k categories from train set
+    top_k = X_train[colname].value_counts()[:k].index
+    # create dummies
+    for cat in top_k:
+        X_train[cat] = np.where(X_train[colname] == cat, 1, 0)
+        X_test[cat] = np.where(X_test[colname] == cat, 1, 0)
+    X_train['{}_others'.format(colname)] = X_train.apply(
+        lambda x: 0 if x[colname] in top_k else 1, axis=1)
+    X_test['{}_others'.format(colname)] = X_test.apply(
+        lambda x: 0 if x[colname] in top_k else 1, axis=1)
+
+
+def get_dummies(X_train, X_test, colname, k):
+    '''
+    Wrap up get_all_dummies and get_top_k_dummies
+    Inputs:
+        X_train: a data frame of training set
+        X_test: a data frame of test set
+        colname: the name of the column
+        k: (int) the value of k
+    Outputs:
+       Create dummies in both train and test set
+    '''
+    # Decide whether this use get all dummies or top k
+    if len(X_train[colname].value_counts()) > k:
+        get_top_k_dummies(X_train, X_test, colname, k)
+    else:
+        get_all_dummies(X_train, X_test, colname)
+    return X_train.drop(columns=[colname]), X_test.drop(columns=[colname])
+
---
+++ wrap_up/wrap_up.py
@@ -0,0 +1,50 @@
+from community_mean_imputer import *
+from get_dummy import *
+from modeling import*
+
+
+def wrap_up(X, y, min_year, max_year, loc_column, time_column, categorical_col, k, model_list, clfs, grid, threshold):
+    '''
+
+    :param X:
+    :param y:
+    :param min_year:
+    :param max_year:
+    :param loc_column:
+    :param time_column:
+    :param categorical_col:
+    :param k:
+    :param model_list:
+    :param clfs:
+    :param grid:
+    :param threshold:
+    :return:
+    '''
+    evaluation_results = []
+    for year in range(min_year, max_year + 1):
+        X_train = X[X['year'] <= year]
+        X_test = X[X['year'] == year + 2]
+        y_train = y[X['year'] <= year]
+        y_test = y[X['year'] == year + 2]
+        print(len(X_test), len(X_train))
+
+        #imputation
+        imputer = community_mean_imputer()
+        X_train, X_test = imputer.filled_categorical(X_train, X_test, categorical_col)
+        X_train = imputer.train_regional_mean(X_train, loc_column, time_column)
+        X_test = imputer.transform_test(X_test, loc_column, time_column)
+
+        # Drop year column
+        X_train = X_train.drop(columns=['year'])
+        X_test = X_test.drop(columns=['year'])
+
+        #get dummies
+        for col in categorical_col:
+            X_train, X_test = get_dummies(X_train, X_test, col, k)
+
+
+        #run models
+        df = run_models(model_list, clfs, grid, X_train, X_test, y_train, y_test, threshold)
+        evaluation_results.append(df)
+
+    return evaluation_results
