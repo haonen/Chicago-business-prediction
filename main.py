622f3c9a5cdb512c2bd93aa854e3ac1b72725d9a
Peng Wei <pengwei715@gmail.com>
2019-05-31 21:16:31 -0500

    update the main

---
+++ main.py
@@ -0,0 +1,50 @@
+'''
+Main function for the pipeline
+'''
+from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+import yaml
+from collections import OrderedDict
+from itertools import product
+import logging
+import sys
+import numpy as np
+import argparse
+import os
+from pipeline import *
+import pdb
+
+logger = logging.getLogger('generating models')
+ch = logging.StreamHandler(sys.stdout)
+#fh = logging.FileHandler('../log/debug.log')
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+#fh.setFormatter(formatter)
+logger.addHandler(ch)
+#logger.addHandler(fh)
+logger.setLevel(logging.INFO)
+
+
+def run(args):
+    logger.info("starting to run the pipeline")
+    pdb.set_trace()
+    config = args.config
+    with open (config) as config_file:
+        configs = yaml.safe_load(config_file)
+    io_lst = ['input_file', 'roc_path', 'pr_path','out_path']
+    io_dic = dict(zip(io_lst, configs['io'].values()))
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Do a simple machine learning pipeline, load data, split the data, transform data, build models, run models, get the performace matix results')
+    parser.add_argument('--config', dest='config', help='config file for this run', default ='./test_simple.yml')
+    args = parser.parse_args()
+    run(args)
